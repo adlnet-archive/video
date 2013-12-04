d863537910a75b9f3e60b86a35b505293416406c
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2013-12-04 09:27:41 -0500

    removed old video file

--- xapipopcorn.js
+++ xapipopcorn.js
@@ -1,6 +1,7 @@
 (function(ADL){
+    
     var debug = true;
-    var log = function(message) 
+    var log = function(message)
     {
         if (!debug) return false;
         try
@@ -11,97 +12,116 @@
         catch(e){return false;}
     } 
 
-	function PopcornVideo(player) {
-		var myplayer = player;
+    function PopcornVideo(player) {
+        var myplayer = player;
         var playerID = player.media.id;
-        var firstqhit = false;
-        var midwayhit = false;
-        var thirdqhit = false;
-        var completed = false;
-		var isTracking = true;
-        //youtube videos don't have children
+        var firstQuartileHit = false;
+        var halfwayHit = false;
+        var thirdQuartileHit = false;
+        var isTracking = true;
+
+        // Youtube videos don't have children
         var objectURI = player.media.children[0].src ? player.media.children[0].src : player.media.src;
         var videoActivity = {"id":objectURI, "definition":{"name": {"en-US":playerID}}};
-        //you can edit the actor inside of the wrapper or just include it here
-        var actor = ADL.XAPIWrapper.lrs.actor ? ADL.XAPIWrapper.lrs.actor : 
-            {"account":{"name":"lou", "homePage":"uri:testaccount"}};
+        
+        // Edit the actor inside of the wrapper or just include it here
+        var actor = ADL.XAPIWrapper.lrs.actor ? ADL.XAPIWrapper.lrs.actor :
+            {"account":{"name":"tester", "homePage":"uri:testaccount"}};
 
-         myplayer.on("play", function(){
-            // Same event when first playing for playing after pause-need to differ it like this
-            if (myplayer.roundTime() != 0){
-                console.log('video ' + playerID + ' resumed')            
-                startstuff(false)
-            }
-            else{
-                console.log('video ' + playerID + ' launched')
+        // Play event
+        myplayer.on("play", function(){
+            var currentTime = myplayer.currentTime()
+            // This if is a youtube workaround - when replaying youtube vids the roundTime is the same as the duration
+            if (currentTime == Math.round(myplayer.duration())){
+                log('youtube video ' + playerID + ' launched weird')
                 startstuff(true)
             }
+            // Playing after pause
+            else if (currentTime != 0){
+                log('video ' + playerID + ' resumed')
+                startstuff(false)
+            }
+            // Normal start when vid is launched
+            else{
+                log('video ' + playerID + ' launched')
+                startstuff(true)
+            }
+        });
+
+        // Every second event
+        myplayer.on("timeupdate", function(){
+            var currentTime = myplayer.roundTime()
+            // If stmt to catch specific times - the hits get reset to false when video ends
+            if (!firstQuartileHit && currentTime == Math.round(myplayer.duration() * .25)){
+                log('video ' + playerID + ' first point')
+                firstQuartileHit = true
+                middleStuff("firstquartile", currentTime)
+            }
+            else if (!halfwayHit && currentTime == Math.round(myplayer.duration() * .5)){
+                log('video ' + playerID + ' half point')
+                halfwayHit = true
+                middleStuff("halfway", currentTime)
+            }
+            else if (!thirdQuartileHit && currentTime == Math.round(myplayer.duration() * .75)){
+                log('video ' + playerID + ' third point')
+                thirdQuartileHit = true
+                middleStuff("thirdquartile", currentTime)
+            }
+        });
+
+        // Pause event
+        myplayer.on("pause", function(){
+            var currentTime = myplayer.roundTime()
+            // If stmt is a youtube workaround - youtube vids pause right before they end
+            if (currentTime != Math.round(myplayer.duration())){
+                log('video ' + playerID + ' paused')
+                pauseStuff(currentTime)
+            }
          });
 
-         // if stmt to catch specific times - the hits get reset to false when video ends
-         myplayer.on("timeupdate", function(){
-            if (!firstqhit && myplayer.roundTime() == Math.round(myplayer.duration() * .25)){
-                console.log('video ' + playerID + ' first point')
-                firstqhit = true
-                middlestuff("firstquartile", myplayer.roundTime())
+        // Seeked event
+        myplayer.on("seeked", function(){
+            var currentTime = myplayer.roundTime()
+            // If try to replay movie, instead of playing it fires seeked
+            if (currentTime != 0){
+                log('seeked ' + playerID + ' to ' + currentTime)
+                seekStuff(currentTime)
             }
-            else if (!midwayhit && myplayer.roundTime() == Math.round(myplayer.duration() * .5)){
-                console.log('video ' + playerID + ' half point')
-                midwayhit = true
-                middlestuff("midway", myplayer.roundTime())
+            // Youtube workaround - videos seek to 0 when replayed or launched
+            else{
+                log('seeked to 0s so really launched ' + playerID)
+                startstuff(true)
+                // Reset video quartile states
+                firstQuartileHit = halfwayHit = thirdQuartileHit = false;
             }
-            else if (!thirdqhit && myplayer.roundTime() == Math.round(myplayer.duration() * .75)){
-                console.log('video ' + playerID + ' third point')
-                thirdqhit = true
-                middlestuff("thirdquartile", myplayer.roundTime())
-            }
-         });
+        });
 
-         myplayer.on("pause", function(){
-            //popcorn pauses before ends so only record pause when it's not the end
-            if (myplayer.roundTime() != Math.round(myplayer.duration())){
-                console.log('video ' + playerID + ' paused')
-                pausestuff(myplayer.roundTime())                
-            }
-         });
-
-         myplayer.on("seeked", function(){
-            //if try to replay movie, instead of playing it fires seeked
-            if (myplayer.roundTime() != 0){
-                console.log('user seeked ' + playerID + ' to ' + myplayer.roundTime())
-                seekstuff(myplayer.roundTime())
-            }
-         });
-
-         myplayer.on("ended", function(){
-            console.log('video ' + playerID + ' ended')
-            endstuff(myplayer.duration())
-         });    
+        // Ended event
+        myplayer.on("ended", function(){
+            log('video ' + playerID + ' ended')
+            endStuff(myplayer.duration())
+        });    
 
         function startstuff(launched){
-            //reporting for first starting video
+            stmt = {"actor":actor, "object": videoActivity}
             if (launched){
-                report({"actor":actor, 
-                        "verb":ADL.verbs.launched, 
-                        "object":videoActivity});
+                stmt["verb"] = ADL.verbs.launched
             }
-            //reporting for resuming video
             else{
-                var resumeTime = "PT"+Math.round(myplayer.roundTime()) + "S";
-                report({"actor":actor, 
-                        "verb":ADL.verbs.resumed, 
-                        "object":videoActivity, 
-                        "result":{"extensions":{"resultExt:resumed":resumeTime}}});
+                var resumeTime = "PT" + myplayer.roundTime() + "S";
+                stmt["verb"] = ADL.verbs.resumed
+                stmt["result"] = {"extensions":{"resultExt:resumed":resumeTime}}
             }
+            report(stmt);
         }
 
-        function middlestuff(quartile, benchTime) {
+        function middleStuff(quartile, benchTime) {
             var benchObj = {"id":objectURI + "#" + quartile, "definition":{"name":{"en-US":playerID + "#" + quartile}}};
-            var bench = "PT"+Math.round(benchTime) + "S";
+            var bench = "PT" + benchTime + "S";
             var extKey = "resultExt:" + quartile
             var result = {"extensions":{}};
-            result["extensions"][extKey] = bench
             var context = {"contextActivities":{"parent" : [videoActivity]}};
+            result["extensions"][extKey] = bench
             report({"actor":actor,
                     "verb":ADL.verbs.progressed,
                     "object":benchObj,
@@ -109,29 +129,30 @@
                     "context":context});
         }
 
-        function pausestuff(pauseTime){
-            var paused = "PT"+Math.round(pauseTime) + "S";
+        function pauseStuff(pauseTime){
+            var paused = "PT" + pauseTime + "S";
             report({"actor":actor, 
-                    "verb":ADL.verbs.suspended, 
+                    "verb":ADL.verbs.suspended,
                     "object":videoActivity, 
                     "result":{"extensions":{"resultExt:paused":paused}}});
         }
 
-        function seekstuff(seekTime){
-            var seeked = "PT"+Math.round(seekTime) + "S";          
+        function seekStuff(seekTime){
+            var seeked = "PT" + seekTime + "S";
             report({"actor":actor, 
-                    "verb":ADL.verbs.interacted, 
+                    "verb":ADL.verbs.interacted,
                     "object":videoActivity, 
                     "result":{"extensions":{"resultExt:seeked": seeked}}});
         }
 
-        function endstuff(endTime) {
-            var duration = "PT"+Math.round(endTime) + "S";
+        function endStuff(endTime) {
+            var duration = "PT" + Math.round(endTime) + "S";
             report({"actor":actor, 
                     "verb":ADL.verbs.completed, 
                     "object":videoActivity, 
                     "result":{"duration":duration, "completion": true}});
-            firstqhit = midwayhit = thirdqhit = false;
+            // Reset video quartile states
+            firstQuartileHit = halfwayHit = thirdQuartileHit = false;
         }
 
         this.report = report;
@@ -155,12 +176,13 @@
     };
 
     XAPIVideo.prototype.addVideo = function(player) {
-        log("adding player: ");
-        var playerID = player.media.id
         try{
+            var playerID = player.media.id
             var v = new PopcornVideo(player)
         }
-        catch(e){return false;}
+        catch(e){
+            throw "Cannot add video: " + e.message;
+        }        
         this._videos[playerID] = v;
         return true;
     };
@@ -177,13 +199,11 @@
     XAPIVideo.prototype.getVideos = function() {
         var v = []
         for (var k in this._videos) {
-            if (!this._videos.hasOwnProperty(k)) 
-                continue;
             v.push(k);
         }
         return v;
     }
 
-    ADL.XAPIVideo = new XAPIVideo();    
+    ADL.XAPIVideo = new XAPIVideo();
 
 }(window.ADL = window.ADL || {}));
